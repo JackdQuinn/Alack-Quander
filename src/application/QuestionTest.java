b63dc6a3c44850f0eaf9902d7c4513080982bacc
JackDQuinn <JackDQuinn@gmail.com>
2019-05-12 18:30:55 -0500

    Create QuestionTest.java
    generate some tests for the question class

---
+++ src/application/QuestionTest.java
@@ -0,0 +1,34 @@
+package application;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class QuestionTest extends Question {
+
+	@Test
+	void testGetQuestion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAnswer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetWrong1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetWrong2() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetWrong3() {
+		fail("Not yet implemented");
+	}
+
+}

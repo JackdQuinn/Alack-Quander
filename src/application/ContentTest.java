379966bea7e65b1d6f3b8418c05bf62c356a82fe
aorlander <aborlander@gmail.com>
2019-05-12 19:46:14 -0400

    question update

---
+++ src/application/ContentTest.java
@@ -0,0 +1,24 @@
+package application;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ContentTest {
+
+	@Test
+	void testContent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetText() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetText() {
+		fail("Not yet implemented");
+	}
+
+}

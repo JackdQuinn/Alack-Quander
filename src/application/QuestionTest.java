48b7cc98d66eb3005f58acb108e3da67be056b61
JackDQuinn <JackDQuinn@gmail.com>
2019-05-12 19:05:02 -0500

    Update QuestionTest.java

--- src/application/QuestionTest.java
+++ src/application/QuestionTest.java
@@ -5,10 +5,18 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class QuestionTest extends Question {
-	Question q = new Question("MC", "Which way is up?", "answer", "wrong1", "wrong1", "wrong1");
+	public QuestionTest(String questionType, String question, String answer, String wrong1, String wrong2,
+			String wrong3) {
+		super(questionType, question, answer, wrong1, wrong2, wrong3);
+		// TODO Auto-generated constructor stub
+	}
 
+	Question q = new Question("MC", "Which way is up?", "answer", "wrong1", "wrong1", "wrong1");
+	
 	@Test
-	void testGetQuestion(String String) {
+	void testGetQuestion() {
+		System.out.println(q.getQuestion());
+		assertEquals(q.getQuestion(),"Which way is up?");
 		
 	}
 
